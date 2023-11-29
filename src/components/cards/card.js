fdee4983981a6e5901bf74182880ef079c1c9bda
jmcglone <jmcglone@umich.edu>
2023-11-29 13:36:14 -0500

    change positioning of card buttons

--- src/components/cards/card.js
+++ src/components/cards/card.js
@@ -14,17 +14,15 @@ const Card = ({cardImage, card}) => {
     <div className="card col-lg-5 mb-3">      
       <img src={cardImage} alt={`${title}`} className="card-img" />
       <div className="card-body row">
-        <div className="col-lg-8">
+        <div className="col-lg-12">
           <h3 className="card-title">{title}</h3>
           <p className="card-text">{description}</p>
-        </div>
-        <div className="col-lg-4">
           {
             buttonUrl.startsWith("/")
             ? <Link className="card-link btn btn-secondary" role="button" to={buttonUrl}>{buttonLabel}</Link>
             : <a className="card-link btn btn-secondary" role="button" href={buttonUrl}>{buttonLabel}</a>
           }
-        </div>
+        </div>        
       </div>
     </div>
   )
