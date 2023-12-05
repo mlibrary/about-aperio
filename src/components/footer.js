c790b8de5d50579247781830e2954bde248f2ead
jmcglone <jmcglone@umich.edu>
2023-12-05 14:37:41 -0500

    change styling for x logo

--- src/components/footer.js
+++ src/components/footer.js
@@ -27,7 +27,7 @@ const Footer = () => {
             <span className="d-block"><a href="mailto:publish@virginia.edu">publish@virginia.edu</a></span>            
             <a href="https://library.virginia.edu" className="logo-img"><img src="/assets/uva_library_light.gif" alt="University of Virginia Library" /></a>
             <span className="d-block">
-              <a href="https://twitter.com/AperioUVA"><svg height="16px" width="auto" viewBox="0 0 1200 1227" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <a className="twitter" href="https://twitter.com/AperioUVA"><svg height="16px" width="auto" viewBox="0 0 1200 1227" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z" fill="white"/>
 </svg></a> 
               <a className="footer-email text-light" href="http://eepurl.com/dhJ1S1">Join our mailing list</a>
